1aade0446585e7cf3ca2eaf2220019ad253132f6
MisaghMomeniB <jenabemisagh@gmail.com>
2025-01-13 20:13:09 +0330

    Imports

--- main.go
+++ main.go
@@ -1,2 +1,15 @@
 // Go System Monitoring Tool
 // This program monitors CPU, memory, and disk usage and serves the results via an HTTP server
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
+)
